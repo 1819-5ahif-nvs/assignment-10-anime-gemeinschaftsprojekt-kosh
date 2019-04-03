585b9b6b4f72cefcac6d64126d3720fab71caf0c
Peter Hauer <pethaudi@yahoo.de>
2019-04-03 10:43:56 +0200

    created data-comp

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,3 +3,4 @@ import RouterComp from './router-comp/router-comp';
 import CreateComp from './create-comp/create-comp';
 import ReadComp from './read-comp/read-comp';
 import UpdateComp from './update-comp/update-comp';
+import DataComp from './read-comp/data-comp/data-comp';
---
+++ frontend/src/read-comp/data-comp/data-comp.js
@@ -0,0 +1,31 @@
+import html from './data-comp.html';
+import css from './data-comp.css';
+
+export default class DataComp extends HTMLElement {
+	constructor() {
+		super();
+
+		this.root = this.attachShadow({mode: "closed"})
+	}
+
+	connectedCallback() {
+		this.appendHTML();
+		this.addFunctionality();
+	}
+
+	appendHTML() {
+		this.root.innerHTML = html;
+		this.appendCSS();
+	}
+
+	appendCSS() {
+		let style = document.createElement("style");
+		style.innerText = css;
+		this.root.append(style);
+	}
+
+	addFunctionality() {
+	}
+}
+
+customElements.define("data-comp", DataComp);
