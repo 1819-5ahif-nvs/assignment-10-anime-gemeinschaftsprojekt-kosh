f28edd3228e0834f513cf691866bd1c469617441
Peter Hauer <pethaudi@yahoo.de>
2019-04-03 12:16:07 +0200

    create delete-comp

---
+++ frontend/src/delete-comp/delete-comp.js
@@ -0,0 +1,31 @@
+import html from './delete-comp.html';
+import css from './delete-comp.css';
+
+export default class DeleteComp extends HTMLElement {
+	constructor() {
+		super();
+
+		this.root = this.attachShadow({mode: "closed"})
+	}
+
+	connectedCallback() {
+		this.appendHTML();
+		this.addFunctionality();
+	}
+
+	appendHTML() {
+		this.root.innerHTML = html;
+		this.appendCSS();
+	}
+
+	appendCSS() {
+		let style = document.createElement("style");
+		style.innerText = css;
+		this.root.append(style);
+	}
+
+	addFunctionality() {
+	}
+}
+
+customElements.define("delete-comp", DeleteComp);
