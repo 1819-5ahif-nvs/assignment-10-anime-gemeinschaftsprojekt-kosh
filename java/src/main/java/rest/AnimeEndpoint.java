18d89119da8698316501dfa27b3ddb65a22c05cf
Peter Hauer <pethaudi@yahoo.de>
2019-04-07 19:31:57 +0200

    fixed server issues

--- java/src/main/java/rest/AnimeEndpoint.java
+++ java/src/main/java/rest/AnimeEndpoint.java
@@ -41,8 +41,8 @@ public class AnimeEndpoint {
     */
     @POST
     @ApiOperation(value = "Add Anime")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response addAnime(@ApiParam(value = "jsonAnime", required = true) Anime a) throws ParseException {
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response addAnime(@ApiParam(value = "jsonAnime", required = true) Anime a) {
         animeFacade.save(a);
         return Response.ok().status(201)
                 .build();
@@ -61,19 +61,13 @@ public class AnimeEndpoint {
     */
     @PUT
     @ApiOperation(value = "Update Anime")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response updateAnime(@ApiParam(value = "jsonAnime", required = true) JSONObject json) throws ParseException {
-        SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
-        Anime anime = animeFacade.getAnimeById(json.getLong("episodeId"));
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response updateAnime(@ApiParam(value = "jsonAnime", required = true) Anime a) {
 
+        System.out.println(a.getTitle());
+        Anime anime = animeFacade.getAnimeById(a.getEpisodeId());
         if(anime != null) { //check if Anime with that id really exists
-
-            anime.setTitle(json.getString("title"));
-            anime.setAiredFrom(sdf.parse(json.getString("airedFrom")));
-            anime.setAiredTo(sdf.parse(json.getString("airedTo")));
-            anime.setVideoURL(json.getString("videoURL"));
-            anime.setForumURL(json.getString("forumURL"));
-            animeFacade.save(anime);
+            animeFacade.save(a);
 
             return Response.ok()
                     .build();
