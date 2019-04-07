9aa2a819b8fca9c018db03f4cddfe6c13f0d7f89
Peter Hauer <pethaudi@yahoo.de>
2019-04-07 12:09:46 +0200

    implemented dateworker

--- frontend/src/read-comp/data-comp/data-comp.js
+++ frontend/src/read-comp/data-comp/data-comp.js
@@ -8,7 +8,7 @@ export default class DataComp extends HTMLElement {
 		this.root = this.attachShadow({mode: "closed"})
 	}
 
-	get episodeId() {
+	get episodeid() {
 		return this.getAttribute("episodeid");
 	}
 
@@ -16,25 +16,51 @@ export default class DataComp extends HTMLElement {
 		return this.getAttribute("title");
 	}
 	
-	get airedFrom() {
-		return this.getAttribute("airedfrom");
+	get airedfrom() {
+		return this.dateParser(this.getAttribute("airedfrom"));
 	}
 
 	get airedto() {
-		return this.getAttribute("airedto");
+		return this.dateParser(this.getAttribute("airedto"));
+	}
+
+	get videourlShorted() {
+		const data = this.getAttribute("videourl")
+		return data.slice(data.length - 10, data.length);
 	}
 
 	get videourl() {
 		return this.getAttribute("videourl");
 	}
 
+	get forumurlShorted() {
+		const data = this.getAttribute("forumurl")
+		return data.slice(data.length - 10, data.length);
+	}
+
 	get forumurl() {
 		return this.getAttribute("forumurl");
 	}
 
+	dateParser(date) {
+		//Oct 24, 1998 12:00:00 AM
+		if(date === "Jan 1, 0001 12:00:00 AM")
+			return "no date";
+		const splits = date.split(" ");
+		return splits[0] + " " + splits[1] + " " + splits[2];
+	}
+
 	connectedCallback() {
 		this.appendHTML();
-		this.addFunctionality();
+
+		this.root.querySelector("#episodeid").innerText = this.episodeid;
+		this.root.querySelector("#title").innerText = this.title;
+		this.root.querySelector("#airedfrom").innerText = this.airedfrom;
+		this.root.querySelector("#airedto").innerText = this.airedto;
+		this.root.querySelector("#videourl").innerText = this.videourlShorted;
+		this.root.querySelector("#videourl").href = this.videourl;
+		this.root.querySelector("#forumurl").innerText = this.forumurlShorted;
+		this.root.querySelector("#forumurl").href = this.forumurl;
 	}
 
 	appendHTML() {
@@ -46,10 +72,6 @@ export default class DataComp extends HTMLElement {
 		let style = document.createElement("style");
 		style.innerText = css;
 		this.root.append(style);
-	}
-
-	addFunctionality() {
-		console.log(this.videourl);
 	}
 }
 
