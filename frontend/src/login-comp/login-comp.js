076d374faf4a97519acbd5d62040ceb7bec51022
Peter Hauer <pethaudi@yahoo.de>
2019-04-08 20:16:35 +0200

    created login-comp

---
+++ frontend/src/login-comp/login-comp.js
@@ -0,0 +1,31 @@
+import html from './login-comp.html';
+import css from './login-comp.css';
+
+export default class  LoginComp extends HTMLElement {
+	constructor() {
+		super();
+
+		this.root = this.attachShadow({mode: "closed"})
+	}
+
+	connectedCallback() {
+		this.appendHTML();
+		this.addFunctionality();
+	}
+
+	appendHTML() {
+		this.root.innerHTML = html;
+		this.appendCSS();
+	}
+
+	appendCSS() {
+		let style = document.createElement("style");
+		style.innerText = css;
+		this.root.append(style);
+	}
+
+	addFunctionality() {
+	}
+}
+
+customElements.define("login-comp", LoginComp);
