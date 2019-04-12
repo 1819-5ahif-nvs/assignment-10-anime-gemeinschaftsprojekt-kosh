50bf0ca6ff20e630ba1aed21abacbb2764b00cba
Peter Hauer <pethaudi@yahoo.de>
2019-04-12 09:40:09 +0200

    commit before merge

--- frontend/src/update-comp/update-comp.js
+++ frontend/src/update-comp/update-comp.js
@@ -44,59 +44,31 @@ export default class UpdateComp extends HTMLElement {
 	}
 
 	dateParser(date) {
-		const splits = date.split(" ");
-		return splits[2] + "-" + this.getMonth(splits[0]) + "-" + this.getDay(splits[1].split(",")[0]);
+		if(date.year == "9999")
+			return "0001-01-01";
+
+		return date.year + "-" + this.getNum(date.month) + "-" + this.getNum(date.day);
 	}
 
-	getMonth(num) {
-		if(num == "Jan") {
-			return "01";
-		}
-		else if(num == "Feb") {
-			return "02";
-		}
-		else if(num == "Mar") {
-			return "03";
-		}
-		else if(num == "Apr") {
-			return "04";
-		}
-		else if(num == "May") {
-			return "05";
-		}
-		else if(num == "Jun") {
-			return "06";
-		}
-		else if(num == "Jul") {
-			return "07";
-		}
-		else if(num == "Aug") {
-			return "08";
-		}
-		else if(num == "Sep") {
-			return "09";
-		}
-		else if(num == "Oct") {
-			return "10";
-		}
-		else if(num == "Nov") {
-			return "11";
-		}
-		else if(num == "Dec") {
-			return "12";
-		}
-	}
-
-	getDay(num) {
-		if(num.length > 1) {
+	getNum(num) {
+		if(num > 9) {
 			return num;
 		}
-		else if(num.length == 1) {
+		else if(num > 0) {
 			return "0" + num;
 		}
-		else if(num.length == 0) {
+		else {
 			return "00";
 		}
+	}
+
+	getDate(date) {
+		const split = date.split("-");
+		return new Date(split[0], split[1], split[2], 0, 0, 0, 0).toLocaleDateString(); /* {
+			year: split[0],
+			month: split[1],
+			day: split[2]
+		}; */
 	}
 
 	appendCSS() {
@@ -110,10 +82,12 @@ export default class UpdateComp extends HTMLElement {
 			let elem = {};
 			elem.episodeId = this.current.episodeId
 			elem.title = this.elem("#title").value;
-			elem.airedFrom = this.elem("#airedfrom").value;
-			elem.airedTo = this.elem("#airedto").value;
+			elem.airedFrom = this.getDate(this.elem("#airedfrom").value);
+			elem.airedTo = this.getDate(this.elem("#airedto").value);
 			elem.videoURL = this.elem("#video").value;
 			elem.forumURL = this.elem("#forum").value;
+
+			console.log(elem.airedFrom);
 			
 			this.worker.update(elem);
 
