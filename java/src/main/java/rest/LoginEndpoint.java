b17d8b09a60a3ada4dca535968580ab9d0be0838
Peter Hauer <pethaudi@yahoo.de>
2019-04-15 12:46:46 +0200

    merge commit

--- java/src/main/java/rest/LoginEndpoint.java
+++ java/src/main/java/rest/LoginEndpoint.java
@@ -1,35 +1,40 @@
 package rest;
 
-import business.UserRepository;
+import business.UserFacade;
 import entities.User;
+import entities.UserDTO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import rest.jwt.JWTCredential;
 import rest.jwt.JWTStore;
 
 import javax.inject.Inject;
-import javax.json.JsonObject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.MediaType;
 
 @Path("login")
+@Api(value="Login")
 public class LoginEndpoint {
     @Inject
-    private UserRepository repo;
+    private UserFacade userFacade;
     @Inject
     private JWTStore jwtStore;
 
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response login(JsonObject credentials){
-        User user = repo.find(credentials.getString("username"));
-        if(user != null && user.getPassword().equals(credentials.getString("password"))) {
+    @Produces(MediaType.APPLICATION_JSON)
+    @ApiOperation(value = "Get the JWT")
+    public Response login(@ApiParam(value = "JsonCredentials", required = true) UserDTO credentials){
+        User user = userFacade.find(credentials.getUsername());
+        if(user != null && user.getPassword().equals(credentials.getPassword())) {
             JWTCredential jwt = jwtStore.generateJWT(user);
             return Response.ok()
-                    .header(HttpHeaders.AUTHORIZATION, "BEARER " + jwt.getValue())
-                    .entity("{\"data\":\"" + jwt.getValue() + "\"}")
+                    .entity("{\"token\":\"" + jwt.getValue() + "\"}")
                     .build();
         }
         else
